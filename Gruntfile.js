a1a8188f447b5d174fcf6e6c993392cae07d78be
Jason Keimig <jason@jkeimig.com>
2016-07-23 01:53:15 -0700

    Sync to 1.3.10

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 		distExports: 'dist',
 		moduleOut: 'glympse-adapter',
 		moduleIn: 'GlympseAdapter',
-		moduleVersion: '1.3.9'
+		moduleVersion: '1.3.10'
       }
     }
   });
--- app/src/VersionInfo.js
+++ app/src/VersionInfo.js
@@ -11,7 +11,7 @@ define(function(require, exports, module)
 	module.exports =
 	{
 		id: 'glympse-adapter',
-		version: '1.3.9',
-		buildDate: '2016-05-18T09:22:44Z'
+		version: '1.3.10',
+		buildDate: '2016-07-23T08:52:33Z'
 	};
 });
