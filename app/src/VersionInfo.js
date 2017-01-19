0d5f99938a835cbc9d9bc82f7ef29e41f7c811de
Jason Keimig <jason@jkeimig.com>
2017-01-19 13:32:47 -0800

    Build 1.5.0b16

--- app/src/VersionInfo.js
+++ app/src/VersionInfo.js
@@ -11,7 +11,7 @@ define(function(require, exports, module)
 	module.exports =
 	{
 		id: 'glympse-adapter',
-		version: '1.5.0b15',
-		buildDate: '2017-01-06T02:32:00Z'
+		version: '1.5.0b16',
+		buildDate: '2017-01-19T21:31:51Z'
 	};
 });
